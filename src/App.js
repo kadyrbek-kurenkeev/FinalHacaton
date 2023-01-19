1cd292e1e9d5fca12ac0b96332314fe4f9bdc7ec
kadyrbek-kurenkeev <kadyrbekkurenkeev0@gmail.com>
2023-01-19 20:35:20 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar";
 import MainRoutes from "./MainRoutes";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <MainRoutes />
+      <Footer />
     </div>
   );
 }
